92a2f0e04e6f797fdcd85674ed0340bf86db8097
GaganTantwar <114756520+GaganTantwar@users.noreply.github.com>
2025-01-08 10:45:48 +0530

    Rectangle.java

--- Day01/Rectangle.java
+++ Day01/Rectangle.java
@@ -3,10 +3,10 @@
 import java.util.*;
 class Rectangle {//class Rectangle
     public static void main(String args[]){ //main function
-	    Scanner sc = new Scanner(System.in); //Scanner class object
-		int length =sc.nextInt();//  input variable for length
-        int width  = sc.nextInt();// input variable for width
-		double perimeter =2*(length+width); // finding perimeter
-		System.out.println("Perimeter of Rectangle: "+ perimeter); // Perimeter of Rectangle
-		}
+	Scanner sc = new Scanner(System.in); //Scanner class object
+	double length =sc.nextInt();//  input variable for length
+        double width  = sc.nextInt();// input variable for width
+	double perimeter =2*(length+width); // finding perimeter
+	System.out.println("Perimeter of Rectangle: "+ perimeter); // Perimeter of Rectangle
+	}
 }
