14b51a06fc257963165b9afe3242c86aaf928a7d
GaganTantwar <panthigagan478@gmail.com>
2025-01-08 10:35:13 +0530

    Distance Km to Miles

---
+++ Day01/Miles.java
@@ -0,0 +1,12 @@
+//Q9  Write a program that takes three numbers as input from the user and printstheir average.
+// Solution
+import java.util.*;
+class Miles {//class Average
+    public static void main(String args[]){ //main function
+	    Scanner sc = new Scanner(System.in); //Scanner class object
+		int dis = sc.nextInt(); //  input variable distance
+        double miles=dis*0.621371; // km to Miles 
+		System.out.println("Distance in Miles "+ miles); 
+		
+		}
+}
