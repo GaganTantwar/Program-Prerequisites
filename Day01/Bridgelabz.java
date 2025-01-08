956b144d1b0527c6bd53b90cc2d3a87670c6c09d
GaganTantwar <panthigagan478@gmail.com>
2025-01-08 10:04:17 +0530

    Welcome to Bridgelabz

---
+++ Day01/Bridgelabz.java
@@ -0,0 +1,8 @@
+//Q1 Write a program that prints "Welcome to Bridgelabz!" to the screen.
+//Solution :
+import java.util.*;
+class Bridgelabz{ //class Bridgelabz
+    public static void main(String args[]){ //main function
+	    System.out.println("Welcome to Bridgelabz!"); //println function of System class 
+		}
+	}
