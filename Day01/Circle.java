8ef5f9c673adb1b427a6aaf26c163cf0b1390b27
GaganTantwar <114756520+GaganTantwar@users.noreply.github.com>
2025-01-08 10:40:45 +0530

    Update Circle.java

--- Day01/Circle.java
+++ Day01/Circle.java
@@ -4,7 +4,7 @@ import java.util.*;
 class Circle {//class Cricle
     public static void main(String args[]){ //main function
 	    Scanner sc = new Scanner(System.in); //Scanner class object
-		int radius = sc.nextInt(); //  input variable for radius of Circle 
+		double  radius = sc.nextDouble(); //  input variable for radius of Circle 
 		double area =  (3.14*radius*radius); // finding area of circle 
 		System.out.println("Area of Circle "+ area); // Area of Circle
 		}
