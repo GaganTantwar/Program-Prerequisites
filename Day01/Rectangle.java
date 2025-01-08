1e2d3316ea42111ac56f8f0bfba6931f6a25acba
GaganTantwar <panthigagan478@gmail.com>
2025-01-08 10:18:11 +0530

    Perimeter of Rectangle

---
+++ Day01/Rectangle.java
@@ -0,0 +1,12 @@
+//Q7   Write a program to calculate the perimeter of a rectangle. Take the length and width as inputs and use the formula: Perimeter = 2 * (length + width).
+// Solution
+import java.util.*;
+class Rectangle {//class Rectangle
+    public static void main(String args[]){ //main function
+	    Scanner sc = new Scanner(System.in); //Scanner class object
+		int length =sc.nextInt();//  input variable for length
+        int width  = sc.nextInt();// input variable for width
+		double perimeter =2*(length+width); // finding perimeter
+		System.out.println("Perimeter of Rectangle: "+ perimeter); // Perimeter of Rectangle
+		}
+}
